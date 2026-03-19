14ac8fb122043acbc0ca2e97e48209b755e8f972
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-18 23:08:13 -0400

    improve forward()

--- transformer/Embedding.py
+++ transformer/Embedding.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 # Map/embed tokens to specific parameters which can contain the information 
 # about position and similarity
@@ -16,6 +17,6 @@ class Embedding(nn.Module):
         B, T = input_ids.shape
         positions = torch.arange(0, T, device=input_ids.device).unsqueeze(0)  # (1, T)
         # the key formula is x = token_embedding + position_embedding
-        x = self.token_embed(input_ids) + self.pos_embed(positions)
+        x = self.token_embed(input_ids) * math.sqrt(self.token_embed.embedding_dim) + self.pos_embed(positions)
         # output_ids: (B, T, d_model)
         return self.dropout(x) 
