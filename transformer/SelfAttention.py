dd3a593ac6c0fe1d4726c82adbdaf471e3484bab
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-18 23:16:06 -0400

    Update SelfAttention.py

--- transformer/SelfAttention.py
+++ transformer/SelfAttention.py
@@ -2,6 +2,7 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import config as *
 
 # For each token, calculate the dependency with other tokens. 
 class SelfAttention(nn.Module):
