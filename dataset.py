2bafe7ac828c5ed2b505a840679093faf0d63c8b
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-20 02:20:04 -0400

    Update dataset.py with stride

--- dataset.py
+++ dataset.py
@@ -2,15 +2,19 @@ import torch
 from torch.utils.data import Dataset
 
 class LMDataset(Dataset):
-    def __init__(self, token_ids, seq_len):
+    def __init__(self, token_ids, seq_len, stride = 64):
         self.token_ids = token_ids
         self.seq_len = seq_len
+        self.stride = stride
 
     def __len__(self):
-        return len(self.token_ids) - self.seq_len
+        if len(self.token_ids) < self.seq_len + 1:
+            return 0
+        return (len(self.token_ids) - self.seq_len - 1) // self.stride + 1
 
     def __getitem__(self, idx):
-        chunk = self.token_ids[idx: idx + self.seq_len + 1]
+        tart = idx * self.stride
+        chunk = self.token_ids[start : start + self.seq_len + 1]
         x = torch.tensor(chunk[:-1], dtype=torch.long)
         y = torch.tensor(chunk[1:], dtype=torch.long)
         return x, y
