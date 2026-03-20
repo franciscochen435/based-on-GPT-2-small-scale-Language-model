419caf6931161eb049510df06539f1323044b34b
Francisco Chen <chenfrancisco435@gmail.com>
2026-03-20 02:20:53 -0400

    Update train.py adding stride

--- train.py
+++ train.py
@@ -142,9 +142,9 @@ def main():
     val_dataset = LMDataset(token_ids_val, max_seq_len)
     test_dataset = LMDataset(token_ids_test, max_seq_len)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, stride=64)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, stride=128)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, stride=128)
 
     run_device = device if torch.cuda.is_available() else "cpu"
 
