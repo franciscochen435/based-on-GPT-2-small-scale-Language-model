357ef006ab13a954943560623995a004251a1250
Francisco Chen <chenfrancisco435@gmail.com>
2026-04-08 17:17:39 -0400

    Simplify Embedding.py

--- transformer/Embedding.py
+++ transformer/Embedding.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import math
 
 # Map/embed tokens to specific parameters which can contain the information 
 # about position and similarity
@@ -8,14 +7,19 @@ import math
 class Embedding(nn.Module):
     def __init__(self, vocab_size, d_model, max_seq_len, dropout=0.15):
         super().__init__()
+        self.max_seq_len = max_seq_len
         self.token_embed = nn.Embedding(vocab_size, d_model)
         self.pos_embed = nn.Embedding(max_seq_len, d_model)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, input_ids):
         # input_ids: (B, T) (Batch size, Sequence length)
-        B, T = input_ids.shape
-        positions = torch.arange(0, T, device=input_ids.device).unsqueeze(0)  # (1, T)
+        _, T = input_ids.shape
+        if T > self.max_seq_len:
+            raise ValueError(
+                f"Input sequence length {T} exceeds max_seq_len {self.max_seq_len}."
+            )
+        positions = torch.arange(T, device=input_ids.device).unsqueeze(0)  # (1, T)
         # the key formula is x = token_embedding + position_embedding
         x = self.token_embed(input_ids) + self.pos_embed(positions)
         # output_ids: (B, T, d_model)
