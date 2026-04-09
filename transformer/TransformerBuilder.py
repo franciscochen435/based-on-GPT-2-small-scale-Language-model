452218e8548c193dd7c11e1166aaaebad917b3b8
Francisco Chen <chenfrancisco435@gmail.com>
2026-04-08 20:57:25 -0400

    Add Builder Pattern for transformer

---
+++ transformer/TransformerBuilder.py
@@ -0,0 +1,50 @@
+from .TransformerModel import TransformerModel
+
+class TransformerModelBuilder:
+    def __init__(self):
+        self._vocab_size = None
+        self._max_seq_len = None
+        self._d_model = None
+        self._n_heads = None
+        self._n_layers = None
+        self._d_ff = None
+        self._dropout = 0.15
+
+    def with_vocab_size(self, n: int):
+        self._vocab_size = n
+        return self
+
+    def with_max_seq_len(self, n: int):
+        self._max_seq_len = n
+        return self
+
+    def with_d_model(self, n: int):
+        self._d_model = n
+        return self
+
+    def with_n_heads(self, n: int):
+        self._n_heads = n
+        return self
+
+    def with_n_layers(self, n: int):
+        self._n_layers = n
+        return self
+
+    def with_d_ff(self, n: int):
+        self._d_ff = n
+        return self
+
+    def with_dropout(self, p: float):
+        self._dropout = p
+        return self
+
+    def build(self):
+        return TransformerModel(
+            vocab_size=self._vocab_size,
+            max_seq_len=self._max_seq_len,
+            d_model=self._d_model,
+            n_heads=self._n_heads,
+            n_layers=self._n_layers,
+            d_ff=self._d_ff,
+            dropout=self._dropout,
+        )
